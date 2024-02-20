ccff0e0d056da93024a70ef30670f08a3aaf25ec
Aleksei Chashchegorov <chashchegorov@gmail.com>
2024-02-21 00:54:04 +0100

    use proper data structure to make code readable

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -1,46 +1,66 @@
 #include "Solution.h"
 
-#include <vector>
 #include <iostream>
+#include <sstream>
+#include <vector>
 
-bool Solution::isMatch(const std::string& r, const std::string& s) {
-	std::vector<std::string_view> simple_regexes;
-	simple_regexes.reserve(r.size());
+using View = std::string_view;
+using Views = std::vector<View>;
 
-	std::cout << r << ":" << s << std::endl;
-	for (const auto& c : r) {
-		if( c == '*' )
-			simple_regexes.back() = std::string_view(simple_regexes.back().begin(), 2);
-		else
-			simple_regexes.emplace_back(std::string_view(&c, 1));
-	}
-
-	std::vector<std::string_view> matched_values;
-	matched_values.reserve(simple_regexes.size());
-
-	for(const auto& sv : simple_regexes) {
-		const char* start = (matched_values.empty()) ? s.data() : matched_values.back().data() + matched_values.back().size();
-		if (start >= (s.data() + s.size())) {
-			matched_values.emplace_back(start, 0);
-			continue;
-		}
-		matched_values.emplace_back(start, sv.size() == 1 ? 1 : s.data() + s.size() - start);
-	}
+struct Pattern {
+	View regex;
+	View string;
 	
-	auto simple_match = [](const std::string_view& r, const std::string_view& s) {
-		if (r == ".*" || (r.size() == 1 && s.size() == 1 && ( r[0] == '.' || r[0] == s[0] )))
+	bool match() const {
+		if (regex == ".*")
 			return true;
-		if (r.size() == 2)
-			return std::count(s.begin(), s.end(), r[0]) == s.size();
+		if (regex.size() == 1 && string.size() == 1 && ( regex[0] == '.' || regex[0] == string[0] ))
+			return true;
+		if (regex.size() == 2)
+			return std::count(string.begin(), string.end(), regex[0]) == string.size();
 		return false;
 	};
-	std::vector<bool> matches(matched_values.size(), false);
-	for (size_t i=0;i<matched_values.size();i++) {
-		const auto& mv = matched_values[i];
-		const auto& sr = simple_regexes[i];
-		matches[i] = simple_match(sr, mv);
-		std::cout << sr << "\t:'" << mv << "'\t" << (matches[i] ? "+" : "-") << std::endl;
-	}	
 
+	std::string str() const {
+		std::stringstream ss;
+		ss << regex << "\t:'" << string << "'\t" << (match() ? "+" : "-");
+		return ss.str();
+	}
+};
+
+using Patterns = std::vector<Pattern>;
+
+bool Solution::isMatch(const std::string& r, const std::string& s) {
+	std::cout << r << ":" << s << std::endl;
+
+	Patterns patterns;
+	patterns.reserve(r.size());
+	
+	for (const auto& c : r) {
+		if( c == '*' )
+			patterns.back().regex = View(patterns.back().regex.begin(), 2);
+		else
+			patterns.emplace_back(Pattern{View(&c, 1),View()});
+
+	}
+	const char* start = s.data();
+	for(auto& pattern : patterns) {
+		if (start >= (s.data() + s.size())) {
+			pattern.string = View(start, 0);
+		} else {
+			pattern.string = View(start, pattern.regex.size() == 1 ? 1 : s.data() + s.size() - start);
+		}
+		start = pattern.string.data() + pattern.string.size();
+	}
+
+	for (auto& pattern : patterns) {
+		std::cout << pattern.str() << std::endl;
+	}
+	
+	auto is_match = std::all_of(patterns.begin(), patterns.end(), [](const auto& pattern) { return pattern.match();} );
+	// !empty && can_move &&  most_+_index 
+	
+	
+	std::cout << is_match << std::endl;
 	return false; // TODO make solution
 }
